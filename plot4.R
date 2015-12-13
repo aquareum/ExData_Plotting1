3416b26efd2e2da3a133ba90f0de96661a845fde
aquareum <beautyjj5@gmail.com>
2015-12-13 17:41:37 -0500

    Complete

--- plot4.R
+++ plot4.R
@@ -8,15 +8,15 @@ new$datetime <- strptime(paste(new$Date,new$Time), "%d/%m/%Y %H:%M:%S")
 windows()
 par(mfrow = c(2,2))
 
-##plot(1)
+#plot(1)
 plot(new$datetime, new$Global_active_power, type = "l", 
      xlab = "", ylab = "Global Active Power")
 
-##plot(2)
+#plot(2)
 plot(new$datetime, new$Voltage, type = "l", xlab = "datetime",
      ylab = "Voltage")
 
-##plot(3)
+#plot(3)
 plot(new$datetime, new$Sub_metering_1, col ="black", type = "l", 
      xlab = "", ylab ="Energy sub metering")
 points(new$datetime, new$Sub_metering_2, col ="red", type = "l")
@@ -25,7 +25,7 @@ points(new$datetime, new$Sub_metering_3, col ="blue", type = "l")
 legend("topright", lwd = 1, col = c("black", "red", "blue"), bty = "n", cex = 0.9,
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-##plot(4)
+#plot(4)
 plot(new$datetime, new$Global_reactive_power, type = "l" ,
      xlab = "datetime", ylab = "Global_reactive_power")
 
